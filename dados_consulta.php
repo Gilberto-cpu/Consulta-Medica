33ff203d600029ec1593218f680e02f80858e639
Gilberto <betomoreno532235@gmail.com>
2024-04-16 18:49:41 -0300

    Segundo commit

---
+++ dados_consulta.php
@@ -0,0 +1,10 @@
+<?php
+$Horario_da_Consulta = $_POST['horario'];
+$Especialidade = $_POST['especialidade'];
+$Nome_do_Medico = $_POST['nomeDoMedico'];
+$Dia_da_Consulta = $_POST['diaDaConsulta'];
+
+echo $Horario_da_Consulta;
+echo $Especialidade;
+echo $Nome_do_Medico;
+echo $Dia_da_Consulta;
